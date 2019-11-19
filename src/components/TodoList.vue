3a070eeb22b15948d7e7caef157fb14f7874c805
bumbum94 <tong940526@gmail.com>
2019-11-19 14:21:52 +0900

    TodoList.vue 기본구조작성

---
+++ src/components/TodoList.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>todo</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TodoList',
+  data(){
+    return {
+      todos: [],
+    }
+  }
+
+}
+</script>
+
+<style>
+
+</style>
